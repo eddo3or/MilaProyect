814fc040aa8416e1317b8ebe5763a9fc788044c4
Felipe Sebastián Pérez Ruelas <felipeperezruelas@gmail.com>
2025-03-26 16:16:12 -0700

    Cambio de ruta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/homepage' element={<Home />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
