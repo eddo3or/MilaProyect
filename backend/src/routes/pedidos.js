25cebcc3a1dcf80c9f5f9eebae0f22d72037d81c
Felipe Sebastián Pérez Ruelas <felipeperezruelas@gmail.com>
2025-03-26 23:40:24 -0700

    Correción del último commit, se agregan los parámetros a las rutas

--- backend/src/routes/pedidos.js
+++ backend/src/routes/pedidos.js
@@ -6,12 +6,12 @@ const router = Router();
 router.get('/get_documentos', Controller.get_documentos);
 
 router.post('/insertar_documento', Controller.insertar_documento);
-router.post('/insertar_subdocumento', Controller.insertar_subdocumento);
+router.post('/insertar_subdocumento/:id', Controller.insertar_subdocumento);
 
-router.put('/actualizar_documento', Controller.actualizar_documento);
-router.put('/actualizar_subdocumento', Controller.actualizar_subdocumento);
+router.put('/actualizar_documento/:id', Controller.actualizar_documento);
+router.put('/actualizar_subdocumento/:id/:id_subdoc', Controller.actualizar_subdocumento);
 
-router.delete('/eliminar_documento', Controller.eliminar_documento);
-router.delete('/eliminar_subdocumento', Controller.eliminar_subdocumento);
+router.delete('/eliminar_documento/:id', Controller.eliminar_documento);
+router.delete('/eliminar_subdocumento/:id/:id_subdoc', Controller.eliminar_subdocumento);
 
 export default router;
--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -7,8 +7,8 @@ router.get('/get_documentos', Controller.get_documentos);
 
 router.post('/insertar_documento', Controller.insertar_documento);
 
-router.put('/actualizar_documento', Controller.actualizar_documento);
+router.put('/actualizar_documento/:id', Controller.actualizar_documento);
 
-router.delete('/eliminar_documento', Controller.eliminar_documento);
+router.delete('/eliminar_documento/:id', Controller.eliminar_documento);
 
 export default router;
