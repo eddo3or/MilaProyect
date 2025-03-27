b8bf2dbcd443e947e3dde89951ecc6b62b8a5f79
Felipe Sebastián Pérez Ruelas <felipeperezruelas@gmail.com>
2025-03-26 18:56:01 -0700

    Capa de servicios para cada modelo

---
+++ backend/src/services/pedidos.js
@@ -0,0 +1,68 @@
+import Modelo from '../models/pedidos';
+
+export const insertar_documento = async (documento) => {
+    try {
+        const doc = new Modelo(documento);
+        return await doc.save();
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const actualizar_documento = async (id, valores) => {
+    try {
+        return await Modelo.findOneAndUpdate({ _id: id }, valores, { new: true, });
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const eliminar_documento = async (id) => {
+    try {
+        return await Modelo.findOneAndDelete({ _id: id });
+    } catch (error) {
+        console.log("Error: ", error.message);
+        return false;
+    }
+}
+
+export const insertar_subdocumento = async (id, subdocumento) => {
+    try {
+        const doc = await Modelo.findByIdAndUpdate(
+            id,
+            { $push: { productos: subdocumento } }
+        );
+        return doc;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const actualizar_subdocumento = async (id, id_subdoc, valores) => {
+    try {
+        const doc = await Modelo.findOneAndUpdate(
+            { _id: id, 'productos._id': id_subdoc },
+            { $set: { 'productos.$': valores } }
+        )
+        return doc;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const eliminar_subdocumento = async (id, id_subdoc) => {
+    try {
+        const deletedItem = await Modelo.findByIdAndUpdate(
+            id,
+            { $pull: { productos: { _id: id_subdoc } } }
+        );
+        return deletedItem;
+    } catch (error) {
+        console.log("Error: ", error.message);
+        return false;
+    }
+}
---
+++ backend/src/services/usuarios.js
@@ -0,0 +1,29 @@
+import Modelo from '../models/usuarios';
+
+export const insertar_documento = async (documento) => {
+    try {
+        const doc = new Modelo(documento);
+        return await doc.save();
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const actualizar_documento = async (id, valores) => {
+    try {
+        return await Modelo.findOneAndUpdate({ _id: id }, valores, { new: true, });
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const eliminar_documento = async (id) => {
+    try {
+        return await Modelo.findOneAndDelete({ _id: id });
+    } catch (error) {
+        console.log("Error: ", error.message);
+        return false;
+    }
+}
