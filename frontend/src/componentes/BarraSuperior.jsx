ed055a6dc2312ad7dcf299bcf7c5a584197cf2aa
Felipe Sebastián Pérez Ruelas <felipeperezruelas@gmail.com>
2025-03-27 01:17:13 -0700

    Mejora a la barra superior

--- frontend/src/componentes/BarraSuperior.jsx
+++ frontend/src/componentes/BarraSuperior.jsx
@@ -1,35 +1,41 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { useUsuarioContext } from '../ContextProvider';
 
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import HomeIcon from '@mui/icons-material/Home';
 
 export default function BarraSuperior({ Texto }) {
+	const { usuario } = useUsuarioContext();
+	const navigate = useNavigate();
+
 	var [date, setDate] = useState(new Date());
-	useEffect( () => {
+	useEffect(() => {
 		var timer = setInterval(() => setDate(new Date(), 1000));
 		return function cleanup() {
 			clearInterval(timer);
 		}
 	});
-	
+
 	return (
 		<div className="centrado-vertical">
-			<div className="centrado-horizontal" style={{justifyContent: 'space-between'}}>
+			<div className="centrado-horizontal" style={{ justifyContent: 'space-between' }}>
 				<p>{date.toLocaleTimeString()}</p>
-				<p>Nombre usuario</p>
+				<p>{usuario.nombre}</p>
 			</div>
 
-			<div className="centrado-horizontal">
+			<div className="centrado-horizontal" style={{ justifyContent: 'space-between' }}>
 				<div>
-					<Texto/>
+					<Texto />
 				</div>
 				<div className="centrado-horizontal">
-					<IconButton>
-						<ArrowBackIcon/>
+					<IconButton onClick={() => navigate(-1)}>
+						<ArrowBackIcon />
 					</IconButton>
-					<IconButton>
-						<HomeIcon/>
+					<IconButton onClick={() => navigate("/home")}>
+						<HomeIcon />
 					</IconButton>
 				</div>
 			</div>
